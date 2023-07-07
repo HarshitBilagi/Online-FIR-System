72307d237aa3111458c2cbbb02766e464f2faad4
Harshit Bilagi <harshit.bilagi@gmail.com>
2023-07-07 14:16:28 +0000

    Updated UI

--- login.php
+++ login.php
@@ -1,27 +1,49 @@
-<?php 
-    $conn = mysqli_connect('localhost','root','','fir');
-	if($conn->connect_error){
-		echo "$conn->connect_error";
-		die("Connection Failed : ". $conn->connect_error);
-	} 
-    if(isset($_POST['login-submit'])){
-        $firstname = $_POST['firstname'];
-        $password = $_POST['password'];
-        $sql = "SELECT * from registration WHERE firstname='$firstname'";
-        $result = mysqli_query($conn, $sql);
-    if($result) {
+<?php
+session_start();
+
+$conn = mysqli_connect('localhost', 'root', '', 'fir');
+if ($conn->connect_error) {
+    die("Connection Failed: " . $conn->connect_error);
+}
+
+if (isset($_POST['login-submit'])) {
+    $firstname = $_POST['firstname'];
+    $password = $_POST['password'];
+
+    // Sanitize user input (example using mysqli_real_escape_string)
+    $firstname = mysqli_real_escape_string($conn, $firstname);
+    $password = mysqli_real_escape_string($conn, $password);
+
+    // Hash the password (example using password_hash)
+    $passwordHash = password_hash($password, PASSWORD_DEFAULT);
+
+    $sql = "SELECT * FROM registration WHERE firstname='$firstname'";
+    $result = mysqli_query($conn, $sql);
+
+    if ($result) {
         $row = mysqli_num_rows($result);
-        if($row > 0) {
+        if ($row > 0) {
             $details = mysqli_fetch_assoc($result);
-            session_start();
-            $_SESSION['firstname'] = $details['firstname'];
-            $_SESSION['userid'] = $details['userid'];
-            $_SESSION['lastname'] = $details['lastname'];
-            $_SESSION['aadhar'] = $details['aadhar'];
-            $_SESSION['number'] = $details['number'];
-            echo "<script>alert('Login Successfull');</script>";
-            header('Location:dashboard.html');
+            $storedPasswordHash = $details['password'];
+
+            // Verify the password (example using password_verify)
+            if (password_verify($password, $storedPasswordHash)) {
+                $_SESSION['firstname'] = $details['firstname'];
+                $_SESSION['userid'] = $details['userid'];
+                $_SESSION['lastname'] = $details['lastname'];
+                $_SESSION['aadhar'] = $details['aadhar'];
+                $_SESSION['number'] = $details['number'];
+                echo "<script>alert('Login Successful');</script>";
+                header('Location:dashboard.html');
+                exit(); // Stop further execution
+            } else {
+                echo "<script>alert('Incorrect password');</script>";
+            }
+        } else {
+            echo "<script>alert('User not found');</script>";
         }
+    } else {
+        echo "<script>alert('Database error');</script>";
     }
-    }
+}
 ?>
